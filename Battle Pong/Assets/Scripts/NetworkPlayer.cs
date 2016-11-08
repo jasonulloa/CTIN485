4c434623dad1e238f9209c455524fbac716c9fc9
Jason Ulloa <jasonull@usc.edu>
2016-11-07 22:03:32 -0800

    Updated stuff.

--- Battle Pong/Assets/Scripts/NetworkPlayer.cs
+++ Battle Pong/Assets/Scripts/NetworkPlayer.cs
@@ -2,12 +2,18 @@
 using System.Collections;
 
 public class NetworkPlayer : Photon.MonoBehaviour {
+	public Transform playerOneSpawnPoint;
+	public Transform playerTwoSpawnPoint;
 
 	void Start () {
 		if (photonView.isMine) {
-
+			GameObject pad1 = (GameObject)Instantiate (Resources.Load ("Paddle"));
+			pad1.transform.position = playerOneSpawnPoint.position;
+			pad1.transform.rotation = playerOneSpawnPoint.rotation;
 		} else {
-
+			GameObject pad2 = (GameObject)Instantiate (Resources.Load ("Paddle"));
+			pad2.transform.position = playerTwoSpawnPoint.position;
+			pad2.transform.rotation = playerTwoSpawnPoint.rotation;
 		}
 	}
 
