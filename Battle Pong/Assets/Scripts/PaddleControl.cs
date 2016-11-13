7c16a86a7c52e392011a97618aa10244e5b409a6
Jason Ulloa <jasonulloa@hotmail.com>
2016-11-13 03:33:53 -0800

    Finally getting networking to work. A lot of functionality is in place.

---
+++ Battle Pong/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PaddleControl : NetworkBehaviour {
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	float speed;
+
+	void Start () {
+		speed = 1.0f;
+	}
+
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow)){
+			if (this.transform.position.z < 42) {
+				this.transform.Translate (0, 0, speed, Space.World);
+			}
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			if (this.transform.position.z > -42) {
+				this.transform.Translate (0, 0, -speed, Space.World);
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			CmdFire ();
+		}
+	}
+
+	[Command]
+	void CmdFire(){
+		GameObject bullet = (GameObject)Instantiate (bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.up * 50.0f;
+		NetworkServer.Spawn (bullet);
+		Destroy (bullet, 3);
+	}
+
+	public override void OnStartLocalPlayer(){
+		GetComponent<MeshRenderer> ().material.color = Color.red;
+	}
+}
